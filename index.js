857e253b0fb9f8c0f1f15e4fe0646a9383c19632
Richard Wilborn <45742979+richardwilborn@users.noreply.github.com>
2022-11-01 11:42:57 -0500

    added array of questions for user input

--- index.js
+++ index.js
@@ -1,7 +1,56 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    name: 'github',
+    message: 'What is your GitHub username?',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address',
+  },
+  {
+    type: 'input',
+    name: 'title',
+    message: "What is your project's name?",
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Please write a short description of your project',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'What kind of license should your project havem, if any?',
+    choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'What command should be run to install any dependencies?',
+    default: 'npm i',
+  },
+  {
+    type: 'input',
+    name: 'test',
+    message: 'What command should be run to run any tests?',
+    default: 'npm test',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'What does the user need to know about using the repo?',
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'What does the user need to know about contributing to the repo?',
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
