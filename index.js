8eaec8aaaee17c6400cd6d88adf7f1055d4a4e53
Richard Wilborn <45742979+richardwilborn@users.noreply.github.com>
2022-11-01 11:49:00 -0500

    updated index.js file

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const path = require('path');
+const inquirer = require("inquirer");
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -53,10 +57,17 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((inquirerResponse) => {
+    console.log('Generating Professional README...');
+    writeToFile('README.md', generateMarkdown({ ...inquirerResponses }));
+  });
+}
 
 // Function call to initialize app
 init();
